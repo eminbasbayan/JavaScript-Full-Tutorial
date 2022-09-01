a8a3244085f44b52ab2a5b854e3654396a263d53
eminbasbayan <emin.basbayan@icloud.com>
2022-09-01 20:36:57 +0300

    değişkenler

---
+++ 2-değişkenler/index.js
@@ -0,0 +1,15 @@
+/* let müdürAdı = "Bedirhan";
+let sınıfBaşkanı = "Emir";
+
+console.log("Müdür Adı:" + müdürAdı);
+console.log("Sınıf Başkanı:" + sınıfBaşkanı);
+
+console.log("Sunum Yapacak Kişiler:", müdürAdı, sınıfBaşkanı);
+ */
+
+const maaş = 7000;
+let zam = 0.1;
+
+let zamlıMaaş = maaş + maaş * zam;
+
+console.log(zamlıMaaş);
