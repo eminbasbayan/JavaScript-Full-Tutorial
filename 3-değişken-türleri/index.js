1409a320cab7b3b3f9fe0546c25405075de321f8
eminbasbayan <emin.basbayan@icloud.com>
2022-09-06 00:33:53 +0300

    değişken türleri

---
+++ 3-değişken-türleri/index.js
@@ -0,0 +1,22 @@
+/* --- 1-String --- */
+// let ogrenciIsmi = "Ahmet Yılmaz";
+// let ogrenciYasi = 25;
+// console.log(typeof ogrenciYasi);
+
+/* --- 2-Number ---  */
+// let sayi1 = 10;
+// let sayi2 = 20;
+// let toplam = Number(sayi1) + Number(sayi2);
+// let toplam = sayi1.toString() + sayi2.toString();
+// console.log(typeof toplam);
+
+/* --- 3-Boolean --- */
+// console.log(typeof false);
+// console.log(typeof true);
+// let sayi1 = 10;
+// let sayi2 = 20;
+// console.log(sayi1 < sayi2);
+
+/* --- 4-Undefined --- */
+// let ogrenciIsmi;
+// console.log(typeof ogrenciIsmi);
