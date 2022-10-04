09825ac80bb23aed818da85736673d990178e2c4
eminbasbayan <emin.basbayan@icloud.com>
2022-10-04 19:37:33 +0300

    if else

---
+++ 6-if-else/index.js
@@ -0,0 +1,27 @@
+
+const username = "eminbasbayan";
+const password = 123456;
+
+
+if(username == "eminbasbayan"){
+    if(password === 123456){
+        console.log("Giriş Yapıldı.");
+    }else{
+        console.log("Şifre Yanlış!");
+    }
+}else{
+    console.log("Kullanıcı Bulunamadı.");
+}
+
+
+
+
+/* 
+let isLoggedIn = false;
+if(isLoggedIn == true){
+    console.log("çalıştı.");
+}else{
+    console.log("true değil");
+}
+
+console.log("Çıktı!"); */
