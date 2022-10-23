447e23e493567fbb5c7f16f40eef0f4b0dfeaebc
eminbasbayan <emin.basbayan@icloud.com>
2022-10-23 12:29:40 +0300

    string metotları

---
+++ 9-string-metotları/index.js
@@ -0,0 +1,14 @@
+let youtube = "Bilgisayar Genetiği";
+
+let result;
+
+result = youtube.toLowerCase();
+result = youtube.toUpperCase();
+result = youtube.length;
+result = youtube.trim();
+result = youtube.slice(0,6);
+result = youtube.split(" ");
+result = youtube[0]
+
+
+console.log(result);
