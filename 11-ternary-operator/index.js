89ea41e2e96c263b1b3d57f03a2054a0f43468b5
eminbasbayan <emin.basbayan@icloud.com>
2022-10-24 12:56:07 +0300

    ternary operator

---
+++ 11-ternary-operator/index.js
@@ -0,0 +1,14 @@
+const username = "eminbasbayan";
+const password = "1234567";
+
+
+password.length > 6 ? console.log("çalıştı") : console.log("çalışmadı.");
+username.length > 12 ? console.log("username doğru") : console.log("doğru değil")
+
+
+/* if(password.length > 6){
+    console.log("çalıştı");
+}else{
+    console.log("6'dan büyük olmadığı için çalışmadı.");
+} */
+
