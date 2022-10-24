7fdefa953e80226dc954a59e38093c907e81c8d3
eminbasbayan <emin.basbayan@icloud.com>
2022-10-24 18:05:39 +0300

    diziler

---
+++ 14-diziler/index.js
@@ -0,0 +1,17 @@
+/* const urun1 = "samsung"; 
+const urun2 = "apple"; 
+const urun3 = "huawei";  */
+
+
+
+
+const urunler = ["samsung", "apple", "huawei"];
+const fiyatlar= [10, 20, 30];
+const urun1 = ["samsung", 10];
+const urun2 = ["apple", 20];
+
+const yeniUrun = ["samsung", 150, ["apple", 20]];
+
+
+
+console.log(`Ürünümüz: ${urunler[2]} ${fiyatlar[2]}`);
