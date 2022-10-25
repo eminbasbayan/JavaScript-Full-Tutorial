f84e27bb6ee8c885b2280349e1402845a97c92fe
eminbasbayan <emin.basbayan@icloud.com>
2022-10-25 11:50:35 +0300

    dizi metotları uygulama

---
+++ 16-dizi-metotları-uygulama/index.js
@@ -0,0 +1,12 @@
+let customers = ["emin", "nur", "emir"];
+
+let customer1 = ["emin", ["asus", "monster", "dell", [10, 20, 30]]];
+
+// console.log(`ürün: ${customer1[1][0]} fiyatı: $${customer1[1][3][0]}`);
+
+// dizinin ilk elemanı
+console.log(customers[0]);
+
+// dizinin son elemanı
+console.log(customers[customers.length - 1]);
+console.log(customers.length - 1);
